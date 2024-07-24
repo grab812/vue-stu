2aa912b4d593159edd351c41a254dd064a2d903f
이아람 <aram.lee5@cj.net>
2024-07-24 16:02:56 +0900

    반응형 컴포넌트 stu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-	<div></div>
+	<div>
+		<h2>반응형 메세지</h2>
+		<p>{{ reactiveMessage }}</p>
+		<button @click="addreactiveMessage">Add Message</button>
+		<h2>일반 메세지</h2>
+		<p>{{ normalMessage }}</p>
+	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const reactiveMessage = ref('Hello Reactive Message');
+const addreactiveMessage = () => {
+	reactiveMessage.value = reactiveMessage.value + '!';
+};
+const normalMessage = 'Hello Normal Message';
+</script>
 
 <style lang="scss" scoped></style>
