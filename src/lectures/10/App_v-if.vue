8061a128be8a104a5545b1d145de28d0e81c1052
rameee <2velyram@gmail.com>
2024-07-29 23:41:21 +0900

    v-if, v-for 강의

---
+++ src/lectures/10/App_v-if.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<h2 v-if="visible">Hello Vue3✌🏻</h2>
+		<h2 v-else>Hello ByeBye👋🏻</h2>
+		<button @click="visible = !visible">toggle</button>
+		<hr />
+		<button @click="type = 'A'">A 버튼</button>
+		<button @click="type = 'B'">B 버튼</button>
+		<button @click="type = 'C'">C 버튼</button>
+		<button @click="type = 'D'">NO 버튼</button>
+		<h2 v-if="type === 'A'">A 입니다</h2>
+		<h2 v-else-if="type === 'B'">B 입니다</h2>
+		<h2 v-else-if="type === 'C'">C 입니다</h2>
+		<h2 v-else>A,B,C가 아닙니다</h2>
+		<hr />
+		<button @click="visible = !visible">listToggle</button>
+		<template v-if="visible">
+			<h1>Title</h1>
+			<p>Paragraph 1</p>
+			<p>Paragraph 2</p>
+		</template>
+		<hr />
+
+		<button @click="ok = !ok">showToggle</button>
+		<h1 v-show="ok">Title</h1>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const visible = ref(true);
+const type = ref('A');
+const ok = ref(false);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/lectures/11/App_v-for.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<ul>
+			<template v-for="(item, index) in evenItems" :key="item.id">
+				<!-- <li v-if="item.id % 2 === 0"> -->
+				<li>id: {{ item.id }} 인덱스 : {{ index }}, {{ item.message }}</li>
+			</template>
+		</ul>
+		<hr />
+		<ul>
+			<li v-for="(value, key, index) in myObject" :key="key">
+				{{ index }}-{{ key }}-{{ value }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script setup>
+import { computed, reactive } from 'vue';
+
+const items = reactive([
+	{ id: 1, message: 'Java' },
+	{ id: 2, message: 'HTML' },
+	{ id: 3, message: 'CSS' },
+	{ id: 4, message: 'Vue3' },
+]);
+const evenItems = computed(() => items.filter(item => item.id % 2 === 0));
+const myObject = reactive({
+	title: '제목',
+	author: '홍길동',
+	publishedAt: '2016-01-01',
+});
+</script>
+
+<style lang="scss" scoped></style>
